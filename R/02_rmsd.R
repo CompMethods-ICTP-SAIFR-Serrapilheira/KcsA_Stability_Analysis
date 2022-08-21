35442740bb946d4d357d05152825740d2a934c72
JSMendivelso <jsmendivelsor@unal.edu.co>
2022-08-21 12:30:39 -0300

    Adding coments and fixing coding style

--- R/02_rmsd.R
+++ R/02_rmsd.R
@@ -1,22 +1,32 @@
+#Root Mean Square Deviation (RMSD) along the trajectory
+
 #Loading necessary packages
 
 library(bio3d)
 
+#Reading the pdb file
 kcsa <- read.pdb("data/raw/kcsa_popcwi.pdb")
+#Reading the trajectory
 kcsa_dcd <- read.dcd("data/raw/kcsa_popcwieq-04.dcd")
-ca.inds <- atom.select(kcsa, elety="CA")
-xyz <- fit.xyz(fixed=kcsa$xyz, mobile=kcsa_dcd,
-               fixed.inds=ca.inds$xyz,
-               mobile.inds=ca.inds$xyz)
-dim(xyz) == dim(kcsa_dcd)
-rd <- rmsd(xyz[1,ca.inds$xyz], xyz[,ca.inds$xyz])
-#plot(rd, xaxt = "n", typ="l", ylab="RMSD",  xlab='Time step (fs)')
-#axis(1, at=0:250, labels=seq(from=0, to=500000, by=2000))
-plot(rd, typ="l", ylab="RMSD(Å)", xlab="Time step")
-points(lowess(rd), typ="l", col="red", lty=2, lwd=1)
+#Vector with the C-alpha atoms in the first frame
+ca <- atom.select(kcsa, elety = "CA")
+#Performing the superposition of each frame with the first one and saving the
+#new coordinates in the xyz matrix
+xyz <- fit.xyz(fixed = kcsa$xyz, mobile = kcsa_dcd,
+               fixed.inds = ca$xyz,
+               mobile.inds = ca$xyz)
+#Calculating RMSD from the initial structure
+rd <- rmsd(xyz[1, ca$xyz], xyz[, ca$xyz])
+#Generating and saving the RMSD plot
+if (!dir.exists("output/figs")) dir.create("output/figs")
 res <- 300
 fig_size <- (res * 240) / 72
 png("output/figs/rmsd.png",
     res = res,
     height = fig_size,
     width = 1.5 * fig_size)
+#Plotting RMSD vs. Time Step
+plot(rd, typ = "l", ylab = "RMSD(Å)", xlab = "Time step")
+#Plotting LOWESS smoothing
+points(lowess(rd), typ = "l", col = "red", lty = 2, lwd = 1)
+dev.off()
--- R/03_rmsf.R
+++ R/03_rmsf.R
@@ -1,13 +1,30 @@
+#Root Mean Square Fluctuations (RMSF) along the trajectory
+
 #Loading necessary packages
 
 library(bio3d)
 
+#Reading the pdb file
 kcsa <- read.pdb("data/raw/kcsa_popcwi.pdb")
+#Reading the trajectory
 kcsa_dcd <- read.dcd("data/raw/kcsa_popcwieq-04.dcd")
-ca.inds <- atom.select(kcsa, elety="CA")
-xyz <- fit.xyz(fixed=kcsa$xyz, mobile=kcsa_dcd,
-               fixed.inds=ca.inds$xyz,
-               mobile.inds=ca.inds$xyz)
-dim(xyz) == dim(kcsa_dcd)
-rf <- rmsf(xyz[,ca.inds$xyz])
-plot(rf, ylab="RMSF (Å)", xlab="Residue number", typ="l", col = "gray12")
+#Vector with the C-alpha atoms in the first frame
+ca <- atom.select(kcsa, elety = "CA")
+#Performing the superposition of each frame with the first one and saving the
+#new coordinates in the xyz matrix
+xyz <- fit.xyz(fixed = kcsa$xyz, mobile = kcsa_dcd,
+               fixed.inds = ca$xyz,
+               mobile.inds = ca$xyz)
+#Calculating RMSF
+rf <- rmsf(xyz[,ca$xyz])
+#Generating and saving the RMSD plot
+if (!dir.exists("output/figs")) dir.create("output/figs")
+res <- 300
+fig_size <- (res * 240) / 72
+png("output/figs/rmsf.png",
+    res = res,
+    height = fig_size,
+    width = 1.5 * fig_size)
+#Plotting RMSF vs. Residue
+plot(rf, ylab = "RMSF (Å)", xlab = "Residue number", typ = "l", col = "gray12")
+dev.off()
